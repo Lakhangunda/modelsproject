17a647d44bf59acc5e7e7fd8bb28e432b2ed564c
Lakhan Gunda <134035982+Lakhangunda@users.noreply.github.com>
2025-08-01 12:01:00 +0530

    Delete modelsapp directory

--- modelsapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    name=models.CharField(max_length=50)
-    age=models.IntegerField()
-    marks=models.IntegerField()
-    avg=models.FloatField()
-    email=models.EmailField()
